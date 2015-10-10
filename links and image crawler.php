2af45ee5c40d10ac376e8fd7fb43a2af955cfd9c
Aryan Jadon <aryanjadon.607@gmail.com>
2015-10-11 01:04:46 +0530

    PHP-Crawler
    it uses simple_html_dom file and php library functions  to fetch the
    links and the images in the target url.

---
+++ links and image crawler.php
@@ -0,0 +1,19 @@
+<?php
+include_once('simple_html_dom.php');
+$target_url = "http://www.ebay.in/sch/i.html?_odkw=computer+science&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR3.TRC0.A0.H0.Xcomputer+science+books.TRS0&_nkw=computer+science+books&_sacat=0";
+$html = new simple_html_dom();
+$html->load_file($target_url);
+$result=array();
+foreach($html->find('#spf_content') as $img)
+{
+   $s1=rtrim($img);
+
+echo $s1;
+array_push($result,"$s1");
+
+
+ 
+}
+
+print_r($result);
+?>
